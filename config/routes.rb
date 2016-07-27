9945a1420d10c93e20d7a95547d7ae596b1e8925
Dhruv Mehra <dmehra6@gatech.edu>
2016-07-27 12:04:39 -0400

    devise backend working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   # devise_for :users
   resources :heros
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'static#show'
-  # get '/app' => 'static#index', via: get
 end

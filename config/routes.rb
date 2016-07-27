b0ea6f9b9a85351b7dcf9c9316ea6062d07e7435
Dhruv Mehra <dmehra6@gatech.edu>
2016-07-27 09:46:19 -0400

    move index.html back to public

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   resources :heros
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static#show'
+  # root 'static#show'
   # get '/app' => 'static#index', via: get
 end

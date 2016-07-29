b5487ed7a7ee0a0f5775c99ba2b39a02327f195c
Dhruv Mehra <dmehra6@gatech.edu>
2016-07-29 15:02:26 -0400

    fix git ignore

--- public/build/models/login-form.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var LoginForm = (function () {
-    function LoginForm() {
-    }
-    return LoginForm;
-}());
-exports.LoginForm = LoginForm;
-//# sourceMappingURL=login-form.js.map

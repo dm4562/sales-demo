da22aa0e68d1dc3135700ec5765bccc296d4c426
Dhruv Mehra <dmehra6@gatech.edu>
2016-08-11 10:52:09 -0400

    update deploy to add buildpacks

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
 # Use this script to deploy the app to heroku.
+# This first sets the required buildpacks then pushes to heroku
 # This will remove old data, populate it with some seed data and create 2 default users
 # admin@useriq.com - useriq16
 # user@userq.com - useriq16
 
+heroku buildpacks:add --index 1 heroku/nodejs
+heroku buildpacks:add --index 2 heroku/ruby
 git push heroku master
 heroku pg:reset DATABASE_URL --confirm sales-seed
 heroku run rails db:migrate

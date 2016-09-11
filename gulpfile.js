3ed4739026461f61c4df207f05bd0de08d3c33b2
Dhruv Mehra <dmehra6@gatech.edu>
2016-09-11 12:40:45 -0400

    test gulpfile again

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var greplace = require('gulp-replace');
 
 var services = 'public/app/src/services';
 var devUrl = 'http://localhost:3001';
-var prodUrl = 'https://' + "sales-seed" + '.herokuapp.com';
+var prodUrl = 'https://' + process.env.SALES_URL + '.herokuapp.com';
 
 gulp.task('prod-url-service', function() {
   return gulp.src([
